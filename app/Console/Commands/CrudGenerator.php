525cb9277cb77d4442cf8407beed9095e1b4e8a3
dsanchezmejia <dsanchez@devolabs.io>
2020-02-09 17:18:34 -0500

    Agragado Codigo Crud Generator

--- app/Console/Commands/CrudGenerator.php
+++ app/Console/Commands/CrudGenerator.php
@@ -6,37 +6,73 @@ use Illuminate\Console\Command;
 
 class CrudGenerator extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'command:name';
 
-    /**
-     * The console command description.
-     *
      * @var string
-     */
-    protected $description = 'Command description';
 
-    /**
-     * Create a new command instance.
-     *
+    protected $signature = 'Crud:Generator {name:Class(singular) for example User}';
+
+
+     * @var string
+
+    protected $description = 'Create Crud Operations';
+
+
      * @return void
-     */
+
     public function __construct()
     {
         parent::__construct();
     }
 
-    /**
-     * Execute the console command.
-     *
      * @return mixed
-     */
-    public function handle()
-    {
-        //
-    }
-}
+
+     public function handle(){
+    $name = $this->argument('name');
+   $this->model($name);
+   $this->controller($name);
+   $this->request($name);
+
+   File::append(base_path('routes/api.php'), "\rRoute::resource('" . Str::plural(strtolower($name)) . "', '{$name}Controller');");
+   Artisan::call('make:migration create_'. strtolower(Str::plural($name)) .'_table --create='. strtolower(Str::plural($name)));
+  }
+
+  protected function getStub($type){
+   return file_get_contents(resource_path("stubs/$type.stub"));
+  }
+
+  protected function model($name){
+   $plantilla = str_replace(
+    ['{{modelName}}']
+    ,[$name]
+    ,$this->getStub('Model')
+   );
+   file_put_contents(app_path("/{$name}.php"), $plantilla);
+  }
+
+  protected function controller($name){
+   $plantilla = str_replace(
+    [
+     '{{modelName}}'
+                ,'{{modelNamePluralLowerCase}}'
+                ,'{{modelNameSingularLowerCase}}'
+    ],[
+     $name
+     ,strtolower(Str::plural($name))
+     ,strtolower($name)
+    ],
+    $this->getStub('Controller')
+   );
+   file_put_contents(app_path("/Http/Controllers/{$name}Controller.php"), $plantilla);
+  }
+  protected function request($name){
+    $plantilla = str_replace(
+       ['{{modelName}}']
+       ,[$name]
+       ,$this->getStub('Request')
+    );
+
+    if(!file_exists($path = app_path('/Http/Requests')))
+       mkdir($path, 0777, true);
+
+    file_put_contents(app_path("/Http/Requests/{$name}Request.php"), $plantilla);
+  }
