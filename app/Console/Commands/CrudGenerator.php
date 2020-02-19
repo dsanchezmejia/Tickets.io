9c7d15bfa575e61cdfd597b775fe779cffcec939
dsanchezmejia <dsanchez@devolabs.io>
2020-02-18 19:10:19 -0500

    Agregado Stubs de Vistas y CrudGenerator

--- app/Console/Commands/CrudGenerator.php
+++ app/Console/Commands/CrudGenerator.php
@@ -7,76 +7,95 @@ use Illuminate\Support\Str;
 use Illuminate\Support\Facades\File;
 use Artisan;
 
-class CrudGenerator extends Command
-{
+class CrudGenerator extends Command{
+    protected $signature = 'crud:generator {name : Class (Singular) for example User}';
+    protected $description = 'Create CRUD Operations';
 
-
-
-    protected $signature = 'Crud:Generator {name : Class (singular) for example User}';
-
-
-
-
-    protected $description = 'Create Crud Operations';
-
-
-
-
-    public function __construct()
-    {
+    public function __construct(){
         parent::__construct();
-    }
+    }//end function
 
+    public function handle(){
+     $name = $this->argument('name');
+    $this->model($name);
+    $this->controller($name);
+    $this->request($name);
+      $this->view($name, 'index');
+      $this->view($name, 'create');
+      $this->view($name, 'edit');
+      $this->view($name, 'header');
 
+    File::append(base_path('routes/web.php'), "\r\nRoute::resource('" . Str::plural(strtolower($name)) . "', '{$name}Controller');");
+    Artisan::call('make:migration create_'. strtolower(Str::plural($name)) .'_table --create='. strtolower(Str::plural($name)));
+   }// end function
 
-     public function handle(){
-    $name = $this->argument('name');
-   $this->model($name);
-   $this->controller($name);
-   $this->request($name);
+   protected function getStub($type){
+    return file_get_contents(resource_path("stubs/$type.stub"));
+   }// end function
 
-   File::append(base_path('routes/api.php'), "\rRoute::resource('" . Str::plural(strtolower($name)) . "', '{$name}Controller');");
-   Artisan::call('make:migration create_'. strtolower(Str::plural($name)) .'_table --create='. strtolower(Str::plural($name)));
-  }
-
-  protected function getStub($type){
-   return file_get_contents(resource_path("stubs/$type.stub"));
-  }
-
-  protected function model($name){
-   $plantilla = str_replace(
-    ['{{modelName}}']
-    ,[$name]
-    ,$this->getStub('Model')
-   );
-   file_put_contents(app_path("/{$name}.php"), $plantilla);
-  }
-
-  protected function controller($name){
-   $plantilla = str_replace(
-    [
-     '{{modelName}}'
-                ,'{{modelNamePluralLowerCase}}'
-                ,'{{modelNameSingularLowerCase}}'
-    ],[
-     $name
-     ,strtolower(Str::plural($name))
-     ,strtolower($name)
-    ],
-    $this->getStub('Controller')
-   );
-   file_put_contents(app_path("/Http/Controllers/{$name}Controller.php"), $plantilla);
-  }
-  protected function request($name){
+   protected function model($name){
     $plantilla = str_replace(
-       ['{{modelName}}']
-       ,[$name]
-       ,$this->getStub('Request')
+     ['{{modelName}}']
+     ,[$name]
+     ,$this->getStub('Model')
     );
+    file_put_contents(app_path("/{$name}.php"), $plantilla);
+   }// end function
 
-    if(!file_exists($path = app_path('/Http/Requests')))
-       mkdir($path, 0777, true);
+   protected function controller($name){
+    $plantilla = str_replace(
+     [
+      '{{modelName}}'
+          ,'{{modelNamePluralLowerCase}}'
+          ,'{{modelNameSingularLowerCase}}'
+     ],[
+      $name
+      ,strtolower(Str::plural($name))
+      ,strtolower($name)
+     ],
+     $this->getStub('Controller')
+    );
+    file_put_contents(app_path("/Http/Controllers/{$name}Controller.php"), $plantilla);
+   }// end function
+   protected function request($name){
+     $plantilla = str_replace(
+        ['{{modelName}}']
+        ,[$name]
+        ,$this->getStub('Request')
+     );
 
-    file_put_contents(app_path("/Http/Requests/{$name}Request.php"), $plantilla);
-  }
-}
+     if(!file_exists($path = app_path('/Http/Requests'))){
+        mkdir($path, 0777, true);
+      }//end if
+     file_put_contents(app_path("/Http/Requests/{$name}Request.php"), $plantilla);
+   }// end function
+
+    protected function view($name, $view){
+      $plantilla = str_replace([
+          '**modelName**'
+          ,'**modelNamePluralLowerCase**'
+          ,'**modelNamePluralucFirst**'
+          ,'**modelNameSingularLowerCase**'
+        ],[
+          $name
+          ,strtolower(Str::plural($name))
+          ,ucfirst(strtolower(Str::plural($name)))
+          ,strtolower($name)
+        ],
+        $this->getStub(ucfirst($view))
+      );
+
+      $name_dir = strtolower(Str::plural($name));
+      if ($view === 'header'){
+        if(!file_exists($path = (resource_path("views/{$name_dir}/partials")))){
+          mkdir($path, 0777, true);
+        }//end if
+        file_put_contents(resource_path("views/{$name_dir}/partials/{$view}.blade.php"), $plantilla);
+      }else{
+        if(!file_exists($path = (resource_path("views/{$name_dir}")))){
+          mkdir($path, 0777, true);
+        }//end if
+        file_put_contents(resource_path("views/{$name_dir}/{$view}.blade.php"), $plantilla);
+      }//end if
+    }// end function
+}//end class
